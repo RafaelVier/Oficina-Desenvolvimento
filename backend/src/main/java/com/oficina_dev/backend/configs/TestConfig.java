a55b057c8bb0bcf9e63e8368644e8f7e348dbef2
ArthurBassani <arthursilvabassani@gmail.com>
2025-06-04 21:13:22 -0300

    feat: create spring profiles, configuration and h2database

---
+++ backend/src/main/java/com/oficina_dev/backend/configs/TestConfig.java
@@ -0,0 +1,24 @@
+package com.oficina_dev.backend.configs;
+
+import com.oficina_dev.backend.models.State.State;
+import com.oficina_dev.backend.Repositories.State.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("test")
+public class TestConfig implements CommandLineRunner {
+    @Autowired
+    private StateRepository stateRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        State state = new State();
+        state.setName("Paraná");
+        state.setAbbreviation("PR");
+        stateRepository.save(state);
+
+    }
+}
