a4472c0ff0b17ca9379b0a95c33028a76f5d7aa5
ArthurBassani <arthursilvabassani@gmail.com>
2025-06-04 21:16:36 -0300

    feat: create the mappers

---
+++ backend/src/main/java/com/oficina_dev/backend/mappers/CategoryMapper.java
@@ -0,0 +1,8 @@
+package com.oficina_dev.backend.mappers;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+
+}
---
+++ backend/src/main/java/com/oficina_dev/backend/mappers/LimitMapper.java
@@ -0,0 +1,7 @@
+package com.oficina_dev.backend.mappers;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface LimitMapper{
+}
