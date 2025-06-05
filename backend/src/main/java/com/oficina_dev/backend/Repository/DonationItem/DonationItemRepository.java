4b92146bc89b3b7cbfbaf7701e03feca785a3a78
ArthurBassani <arthursilvabassani@gmail.com>
2025-06-04 21:21:55 -0300

    fix: delete duplicate classes

--- backend/src/main/java/com/oficina_dev/backend/Repository/DonationItem/DonationItemRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oficina_dev.backend.Repository.DonationItem;
-
-import com.oficina_dev.backend.Models.DonationItem.DonationItem;
-import com.oficina_dev.backend.Models.DonationItem.DonationItemId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DonationItemRepository extends JpaRepository<DonationItem, DonationItemId> {
-}
--- backend/src/main/java/com/oficina_dev/backend/Repository/ReceiverLimit/ReceiverLimitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.oficina_dev.backend.Repository.ReceiverLimit;
-
-import com.oficina_dev.backend.Models.ReceiverLimit.ReceiverLimit;
-import com.oficina_dev.backend.Models.ReceiverLimit.ReceiverLimitId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReceiverLimitRepository extends JpaRepository<ReceiverLimit, ReceiverLimitId> {
-}
